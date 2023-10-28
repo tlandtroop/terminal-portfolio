393225a37fbe1675139f6fff26abf3d89c2da514
Tyler Landtroop <tlandtroop@ufl.edu>
2023-10-27 22:33:04 -0400

    edited commands

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -15,7 +15,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
   @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
+     @@@                        @@@         
         @@@                  @@@ @@           
          @|  @@@@@@@@@@@@@@@@   @@           
          @|                      @@          
